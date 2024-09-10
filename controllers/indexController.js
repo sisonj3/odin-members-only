c0428bfc31760d0e613c7b87018ef13365937c05
sisonj <98570174+sisonj3@users.noreply.github.com>
2024-09-09 18:06:09 -0700

    Added log in authentication

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -4,7 +4,29 @@ const { body, validationResult } = require("express-validator");
 const db = require("../db/queries");
 
 // Check if account with username exists and password is correct
-const validate = [];
+const validate = [
+    body("username").custom(async value => {
+        const user = await db.getUser(value);
+        if (!user) {
+            throw new Error('Username or Email does not exist');
+        }
+    }),
+    body("password").custom(async (value, { req }) => {
+        // Get password associated with username
+        const user = await db.getUser(req.body.username);
+        
+        // If user exists check password
+        if (user) {
+            // Check if passwords match
+            const match = await bcrypt.compare(value, user.password);
+            
+            if (!match) {
+                throw new Error('Password is incorrect');
+            }
+            
+        }
+    }),
+];
 
 // Log user into session after validating account credentials
 const logInUser = [validate, asyncHandler(async (req, res) => {
@@ -13,8 +35,16 @@ const logInUser = [validate, asyncHandler(async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         console.log('Log in failed!');
+        return res.status(400).render("logIn", { errors: errors.array(), });
+    }
+    
+    // If not a member redirect to "/membership/${req.body.username}"
+    const user = await db.getUser(req.body.username);
+
+    if (!user.ismember) {
+        res.redirect(`/membership/${user.username}`);
         return;
-    } // Else if not a member redirect to "/membership/${req.body.username}"
+    }
 
     console.log("Logging in user!");
 })
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,8 +1,13 @@
+const indexController = require('../controllers/indexController');
+
 const { Router } = require("express");
 
 const indexRouter = Router();
 
 // Render views/index.ejs
 indexRouter.get("/", (req, res) => res.render("logIn"));
+
+// Validate login info
+indexRouter.post("/", indexController.logInUser);
 
 module.exports = indexRouter;
